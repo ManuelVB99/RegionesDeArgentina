ecaa507005ad18d27c0965ca4b6d6ea58bc8ec05
Manuel Vilela Blanco <vilelablancomanuel@gmail.com>
2022-10-01 19:33:20 -0300

    Se agregó un archivo de forms para poder agregar formularios

---
+++ Modelos/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.models import User
+
+class editarblogsalta(forms.Form):
+    titulo = forms.CharField(max_length=60)
+    subtitulo = forms.CharField(max_length=100)
+    cuerpo = forms.CharField(widget= forms.Textarea(attrs={'height': '200px'}))
+    autor = forms.CharField(max_length=40)
+    fecha = forms.CharField(max_length=40)
+    image = forms.ImageField()
