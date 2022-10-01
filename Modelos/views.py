a07824d0261462db40fd1dd4fae2265a5964b085
Manuel Vilela Blanco <vilelablancomanuel@gmail.com>
2022-10-01 19:34:29 -0300

    Se agregaron las funciones de crud salta en views

--- Modelos/views.py
+++ Modelos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from Modelos.models import *
+from Modelos.forms import *
 
 def noroeste(request):
     return render(request, "Noroeste/noroeste.html")
@@ -7,6 +8,12 @@ def noroeste(request):
 def blogssalta(request):
     blogsalta = BlogsSalta.objects.all()
     return render(request, "Noroeste/salta.html", {"blogsalta" : blogsalta})
+
+def leerblog(request, blogsalta_id):
+    blog = BlogsSalta.objects.get(id = blogsalta_id)
+    img = blog.image
+    img = img.__str__()
+    return render(request, "Noroeste/crud_salta/leerblog.html", {"blog": blog, "img": img})
 
 def agregarblog(request):
     if request.method == 'POST':
@@ -16,6 +23,31 @@ def agregarblog(request):
         return render(request, "Noroeste/salta.html", {"blogsalta": blogsalta})
     return render(request, "Noroeste/crud_salta/agregarblog.html")
 
+def editarblog(request, blogsalta_id):
+    blog = BlogsSalta.objects.get(id = blogsalta_id)
 
+    if request.method == 'POST':
+        formulario = editarblogsalta(request.POST)
 
+        if formulario.is_valid():
+            informacion = formulario.cleaned_data
+            blog.titulo = informacion['titulo']
+            blog.subtitulo = informacion['subtitulo']
+            blog.cuerpo = informacion['cuerpo']
+            blog.autor = informacion["autor"]
+            blog.fecha = informacion["fecha"]
+            blog.image = informacion["image"]
+            blog.save()
+            blogsalta = BlogsSalta.objects.all()
+            return render(request, "Noroeste/salta.html", {"blogsalta": blogsalta})
+    else:
+        formulario = editarblogsalta(initial={'titulo': blog.titulo, 'subtitulo': blog.subtitulo, 'cuerpo': blog.cuerpo, "autor": blog.autor, "fecha": blog.fecha})
+    return render(request, "Noroeste/crud_salta/editarblog.html", {"formulario": formulario})
+
+def borrarblog(request, blogsalta_id):
+    blog = BlogsSalta.objects.get(id = blogsalta_id)
+    blog.delete()
+
+    blogsalta = BlogsSalta.objects.all()
+    return render(request, "Noroeste/salta.html", {"blogsalta": blogsalta})
 # Create your views here.
